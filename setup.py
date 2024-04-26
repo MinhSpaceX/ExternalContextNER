43dbad856b8163ff819c9dd07657e6fa0dd18c11
bags <bags@aiacademy.edu.vn>
2024-04-26 09:17:41 +0700

    revise code

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+
+setup(name = "externalcontext-ner",
+    version = "1.0",
+    author = "sonba",
+    author_email = "bagiangson2001@gmail.com",
+    packages=find_packages()
+) 
