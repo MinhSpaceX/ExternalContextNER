a5ed3a1b6efc6fd0aaa566ebe87020f5cc7b23e7
MinhSpaceX <126952602+MinhSpaceX@users.noreply.github.com>
2024-10-09 20:22:10 +0700

    Update run_vlsp2016_more.sh

--- run_vlsp2016_more.sh
+++ run_vlsp2016_more.sh
@@ -3,7 +3,7 @@ echo "## Run"
 task_name="sonba"
 learning_rate=3e-5
 num_train_epochs=10
-train_batch_size=2
+train_batch_size=4
 bert_model="FacebookAI/xlm-roberta-large"
 
 # bert_model="vinai/phobert-base-v2"
